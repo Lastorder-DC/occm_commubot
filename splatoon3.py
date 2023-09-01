b92e2a5fc9d276784058e68b5dda7e6aeb192c17
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2023-09-01 06:16:57 +0000

    포맷변경 대응

--- splatoon3.py
+++ splatoon3.py
@@ -64,7 +64,8 @@ def extract_info(type, schedule, locale_db):
         vs_rule = get_rules_name(locale_db, schedule["regularMatchSetting"]["vsRule"]["id"])
     if schedule and "bankaraMatchSettings" in schedule:
         for bankara_match_setting in schedule["bankaraMatchSettings"]:
-            if bankara_match_setting["mode"] != type:
+            print(bankara_match_setting)
+            if bankara_match_setting["bankaraMode"] != type:
                 continue
             vs_stages = [get_stage_name(locale_db, stage["id"]) for stage in bankara_match_setting["vsStages"]]
             vs_images = [stage["image"]["url"] for stage in bankara_match_setting["vsStages"]]
@@ -328,4 +329,4 @@ def get_schedules(locale, target="NOW"):
     }
 
 if __name__ == '__main__':
-    print(get_schedules("ko-KR"))
+    print(get_schedules("ko-KR", "NEXT"))

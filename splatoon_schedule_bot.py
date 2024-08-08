14431bae8cdfe19d613b51d05d8ceda23acc7579
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-08-08 12:54:13 +0900

    debug delete

--- splatoon_schedule_bot.py
+++ splatoon_schedule_bot.py
@@ -499,9 +499,10 @@ def main():
     메인 함수로, Mastodon 스트리밍을 시작합니다.
     """
     toots = m.account_statuses(bot.id)
+    print(toots[0].content.find("봇 가동 시작 : "))
     if toots[0].content.find("봇 가동 시작 : ") != -1:
         m.status_delete(toots[0].id)
-    
+    return
     current_time = datetime.now(timezone('Asia/Seoul'))
     formatted_time = current_time.strftime("%Y년 %m월 %d일") + ' ' + weekday_dict[current_time.weekday()] + ' ' + ampm_dict[current_time.strftime("%p")] + ' ' + current_time.strftime("%I:%M")
     m.status_post(f"봇 가동 시작(v1.0.1) : {formatted_time}", visibility=default_visibility)
